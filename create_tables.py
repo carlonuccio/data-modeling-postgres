34133238b474ac2233aa9a1dd777f22e2319ed7e
carlocciu <carlonuccio91@gmail.com>
2020-03-30 19:04:45 +0200

    PEP8 style guidelines, refactor, docstring, create_database function

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,15 @@
 import psycopg2
 from sql_queries import list_drop_tables, list_create_tables
 
-def insertfromdataframe(hostname, dbname, table, dataframe):
+
+def insert_from_dataframe(hostname, dbname, table, dataframe):
+    """
+    Insert Rows in a db table from a dataframe
+    :param hostname: Host Database Address
+    :param dbname: Database Name
+    :param table: Table Name
+    :param dataframe: Pandas Dataframe
+    """
     conn, cur = db_connection(hostname, dbname)
 
     for index, row in dataframe.iterrows():
@@ -16,7 +24,37 @@ def insertfromdataframe(hostname, dbname, table, dataframe):
     cur.close()
     conn.close()
 
+
+def create_database(hostname, dbname):
+    """
+    - Drop and creates the database
+    :param hostname: Host Database Address
+    :param dbname: Database Name
+    :return: connection conn and a cursor cur
+    """
+
+    # connect to default database
+    conn = psycopg2.connect("host=" + hostname + " dbname=studentdb user=student password=student")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+
+    # create sparkify database with UTF8 encoding
+    cur.execute("DROP DATABASE IF EXISTS " + dbname)
+    cur.execute("CREATE DATABASE " + dbname + " WITH ENCODING 'utf8' TEMPLATE template0")
+
+    # close connection to default database
+    cur.close()
+    conn.close()
+
+
 def db_connection(hostname, dbname):
+    """
+    - Connects to a database
+    - Returns the connection and cursor to database
+    :param hostname: Host Database Address
+    :param dbname: Database Name
+    :return: connection conn and a cursor cur
+    """
     try:
         conn = psycopg2.connect("host="+ hostname + " dbname=" + dbname)
         conn.set_session(autocommit=True)
@@ -32,17 +70,36 @@ def db_connection(hostname, dbname):
 
     return conn, cur
 
+
 def main(hostname, dbname):
+    """
+    - Drops (if exists) and Creates the sparkify database.
+
+    - Establishes connection with the sparkify database and gets
+    cursor to it.
+
+    - Drops all the tables.
+
+    - Creates all tables needed.
+
+    - Finally, closes the connection.
+    :param hostname: Host Database Address
+    :param dbname: Database Name
+    """
+    create_database(hostname, dbname)
     conn, cur = db_connection(hostname, dbname)
 
-    for i_drop in list_drop_tables:
-        cur.execute(i_drop)
+    # Drops each table using the queries in `list_drop_tables` list.
+    for query in list_drop_tables:
+        cur.execute(query)
 
-    for i_create in list_create_tables:
-        cur.execute(i_create)
+    # Creates each table using the queries in `list_create_tables` list.
+    for query in list_create_tables:
+        cur.execute(query)
 
     cur.close()
     conn.close()
+
 
 if __name__ == "__main__":
     main("127.0.0.1", "sparkifydb")
--- etl.py
+++ etl.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import os
-
-from create_tables import insertfromdataframe
+from create_tables import insert_from_dataframe
 from create_tables import db_connection
 import create_tables
 
+
 def process_files(folder):
+    """
+    open all json files within a root folder and append to a pandas dataframe
+    :param folder: path root folder
+    :return: a pandas dataframe
+    """
+    #
     df = pd.DataFrame()
     for root, dirs, files in os.walk(folder):
         for file in files:
@@ -14,37 +20,65 @@ def process_files(folder):
                  df = df.append(df_new, ignore_index=True)
     return df
 
-def process_song_data(hostname, dbname, folder):
 
+def process_song_data(hostname, dbname, folder):
+    """
+    - Process all song data
+    - Subset of artists and copy
+    - Remove duplicates and insert an ID
+    - insert them into database
+    :param hostname: Host Database Address
+    :param dbname: Database Name
+    :param folder: path root folder for song data
+    """
+
+    # process song data
     df = process_files(folder)
 
+    # artists and song subset
     artists = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].copy()
     songs = df[['song_id', 'title', 'artist_id', 'year', 'duration']].copy()
 
+    # convert artist_id to a string, remove duplicates, insert id and insert into a db table
     artists['artist_id'] = artists['artist_id'].astype(str)
+    artists = artists.drop_duplicates(subset='artist_id', keep='first')
     artists.insert(loc=0, column='index', value=range(0, 0 + len(artists)))
-    insertfromdataframe(hostname, dbname, "artists", artists.drop_duplicates(subset='artist_id', keep='first'))
+    insert_from_dataframe(hostname, dbname, "artists", artists)
 
+    # convert song_id to a string, remove duplicates, insert id and insert into a db table
     songs['song_id'] = songs['song_id'].astype(str)
+    songs = songs.drop_duplicates(subset='song_id', keep='first')
     songs.insert(loc=0, column='index', value=range(0, 0 + len(songs)))
-    insertfromdataframe(hostname, dbname, "songs", songs.drop_duplicates(subset='song_id', keep='first'))
+    insert_from_dataframe(hostname, dbname, "songs", songs)
+
 
 def process_log_data(hostname, dbname, folder):
+    """
+    - Process all log data
+    - Subset of users, time and songplays
+    - insert users, time and songplays into database
+    """
 
     df = process_files(folder)
+
+    # filter by NextSong action
     df = df[df.page == "NextSong"]
 
+    # convert timestamp column to datetime
+    df['ts'] = pd.to_datetime(df['ts'], unit='ms')
+
+    # users, time and songplays subset
     users = df[['userId', 'firstName', 'lastName', 'gender', 'level']].copy()
     time = df[['ts']].copy()
     songplays = df[['ts', 'userId', 'level', 'song', 'artist', 'sessionId', 'location', 'userAgent']].copy()
 
-    # USERS
+    # insert user records
     users['userId'] =  users['userId'].astype(str)
-    insertfromdataframe(hostname, dbname, "users", users.drop_duplicates(subset=['userId', 'level']))
+    insert_from_dataframe(hostname, dbname, "users", users.drop_duplicates(subset=['userId', 'level']))
 
-    #### TIME
-    time['ts'] = pd.to_datetime(time['ts'], unit='ms')
 
+
+    # insert time data records
     time_dict = {"start_time": time.ts,
                   "hour": time.ts.dt.hour,
                   "day": time.ts.dt.day,
@@ -54,16 +88,13 @@ def process_log_data(hostname, dbname, folder):
                   "weekday": time.ts.dt.weekday
                   }
     time_df = pd.DataFrame.from_dict(time_dict)
-    insertfromdataframe(hostname, dbname, "time", time_df.drop_duplicates())
+    insert_from_dataframe(hostname, dbname, "time", time_df.drop_duplicates())
 
-    # SONGPLAY
-
+    # insert songplay records
     conn, cur = db_connection(hostname, dbname)
 
-    songplays['ts'] = pd.to_datetime(songplays['ts'], unit='ms')
-
+    # get songid and artistid from song and artist tables
     for index, row in songplays.iterrows():
-        # get songid and artistid from song and artist tables
         sql_select_query = """select idSong from Songs where title = %s"""
         cur.execute(sql_select_query, (row['song'],))
         idSong = cur.fetchone()
@@ -89,16 +120,25 @@ def process_log_data(hostname, dbname, folder):
     cur.close()
     conn.close()
 
+    # insert songplay record
     songplays.insert(loc=0, column='index', value=range(0, 0 + len(songplays)))
-    insertfromdataframe(hostname, dbname, "songplays", songplays)
+    insert_from_dataframe(hostname, dbname, "songplays", songplays)
 
 
 def main():
+    """
+    - Drops (if exists) and Creates the sparkify database and relative tables
+
+    - process song data
+
+    - process log data
+    """
     hostname,dbname = "127.0.0.1","sparkifydb"
     create_tables.main(hostname, dbname)
 
-    process_song_data(hostname, dbname, "./data/song_data/" )
-    process_log_data(hostname, dbname, "./data/log_data/" )
+    process_song_data(hostname, dbname, "./data/song_data/")
+    process_log_data(hostname, dbname, "./data/log_data/")
+
 
 if __name__ == "__main__":
     main()
